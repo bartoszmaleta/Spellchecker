28f8c0b779c9c01793f44829ccc004952fb3ca99
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-11 06:41:46 +0200

    Initial commit with implementation

---
+++ src/main/java/WordChecker.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordChecker
+{
+    /**
+     * Constructor that initializes a new WordChecker with a given WordList.
+     *
+     * @param wordList Initial word list to check against.
+     * @see WordList
+     */
+    public WordChecker(WordList wordList)
+    {
+
+    }
+
+
+    /**
+     * Returns true if the given word is in the WordList passed to the
+     * constructor, false otherwise.
+     *
+     * @param word Word to chack against the internal word list
+     * @return bollean indicating if the word was found or not.
+     */
+    public boolean wordExists(String word)
+    {
+
+    }
+
+
+    /**
+     * Returns an ArrayList of Strings containing the suggestions for the
+     * given word.  If there are no suggestions for the given word, an empty
+     * ArrayList of Strings (not null!) should be returned.
+     *
+     * @param word String to check against
+     * @return A list of plausible matches
+     */
+    public ArrayList getSuggestions(String word)
+    {
+
+    }
+}
